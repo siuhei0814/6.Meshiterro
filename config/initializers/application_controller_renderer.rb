5a070a9bdcbe2364a7d3c86497b1656ddc2b1fe4
siuhei0814 <thewindforest.0814@gmail.com>
2021-08-19 05:26:05 +0000

    [Continue]6-3_config

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7d49d0015ec0f836d404bf27c31813e15589fc1bf231b1c08cbc25c11a02a765277c846e0411038a7cee94d93fd24f999e07b03519b1093f354381d53dce2265'
